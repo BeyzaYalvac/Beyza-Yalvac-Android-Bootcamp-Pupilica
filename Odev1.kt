c4b5334e3a798db2a5e1db4da6b461445fa7e473
BeyzaYalvac <102876353+BeyzaYalvac@users.noreply.github.com>
2025-04-25 16:55:37 +0300

    Add files via upload
    Beyza Yalvaç Pupilica Android Bootcamp 1.Odev

---
+++ Odev1.kt
@@ -0,0 +1,102 @@
+package com.example.androidpupilicabootcamp.degiskenler
+
+//Beyza Yalvaç
+//Pupilica Android Bootcamp - Ödev 1
+
+fun main() {
+    var sehir:String="İzmir"
+    var ulke:String="Türkiye"
+    var telefon:Long=156524534
+    var postaKodu:Int=15415
+    var email:String= "bfb@gmail.com"
+    var meslek:String="Yazılımcı"
+    var stokMiktari:Int=1512
+    var musteriAdi:String="Rubia"
+    var bakiye:Double=12.24
+    var dogumgunu:String="14.05.2025"
+    var maas:Float=50.14f
+    var medeniDurum:Boolean=false
+    var urunYorum:String="Bu rün oldukça kullanışkı ve uzun ömürlüydü."
+    var odemeTarihi:String="15.10.2025"
+    var odeme:String="Kredi kartı"
+    var siparisAdeti:Int=5;
+    var arabaModeli:String="Tesla Model Y"
+    var kitapAdi:String="Ask ve Gurur"
+    var yayinlanmaTarihi:String = "10.10.2025"
+    var indirimMiktari:Double=15.5
+    var odaSayisi:Int=3
+    var enlem:Double=155.21327485
+    var boylam:Double=087.54654515
+    var urunAdi:String="Kulaklık"
+    var yemekFiyati:Double=280.5
+    var marka:String="Guess"
+    var muzikAdi:String="Euphoria"
+    var videoSuresi:Double=3.54
+    var urunPuani:Float=4.5f
+    var resimAdi:String="Mona Lisa"
+    var dosyaFormati:String="mp3"
+    var renk:String="Kırmızı"
+    var renkKodu:String="0xff16Ad"
+    var telefonModeli:String="Iphone 16"
+    var ekranBoyutu:String="16''"
+    var agirlik:String="32kg"
+    var ulusalGun:String="23 Nisan"
+    var tatilGunu:String="Cumartesi"
+    var rezervasyonTarihi:String="13.30.2025"
+    var sokakAdi:String="Mehmet Akif Ersoy Sokak"
+    var otobusHatti:String="72T"
+    var kalanDakika:Double=15.46
+    var takipKodu:String="354kdbv5f456"
+    var kuponSuresi:String="01.01.2025"
+    var kuponKodu:String="s354df45sfd4f53dw"
+    var faturaAdresi:String="abc mahallesi,def sokak,ghj apartmanı,no 34,daire:25, Ataşehir/Istanbul"
+
+
+    println("sehir: $sehir")
+    println("ulke: $ulke")
+    println("telefon: $telefon")
+    println("postaKodu: $postaKodu")
+    println("email: $email")
+    println("meslek: $meslek")
+    println("stokMiktari: $stokMiktari")
+    println("musteriAdi: $musteriAdi")
+    println("bakiye: $bakiye")
+    println("dogumgunu: $dogumgunu")
+    println("maas: $maas")
+    println("medeniDurum: $medeniDurum")
+    println("urunYorum: $urunYorum")
+    println("odemeTarihi: $odemeTarihi")
+    println("odeme: $odeme")
+    println("siparisAdeti: $siparisAdeti")
+    println("arabaModeli: $arabaModeli")
+    println("kitapAdi: $kitapAdi")
+    println("yayinlanmaTarihi: $yayinlanmaTarihi")
+    println("indirimMiktari: $indirimMiktari")
+    println("odaSayisi: $odaSayisi")
+    println("enlem: $enlem")
+    println("boylam: $boylam")
+    println("urunAdi: $urunAdi")
+    println("yemekFiyati: $yemekFiyati")
+    println("marka: $marka")
+    println("muzikAdi: $muzikAdi")
+    println("videoSuresi: $videoSuresi")
+    println("urunPuani: $urunPuani")
+    println("resimAdi: $resimAdi")
+    println("dosyaFormati: $dosyaFormati")
+    println("renk: $renk")
+    println("renkKodu: $renkKodu")
+    println("telefonModeli: $telefonModeli")
+    println("ekranBoyutu: $ekranBoyutu")
+    println("agirlik: $agirlik")
+    println("ulusalGun: $ulusalGun")
+    println("tatilGunu: $tatilGunu")
+    println("agirlik: $agirlik")
+    println("rezervasyonTarihi: $rezervasyonTarihi")
+    println("sokakAdi: $sokakAdi")
+    println("otobusHatti: $otobusHatti")
+    println("kalanDakika: $kalanDakika")
+    println("takipKodu: $takipKodu")
+    println("kuponSuresi: $kuponSuresi")
+    println("kuponKodu: $kuponKodu")
+    println("faturaAdresi: $faturaAdresi")
+}
